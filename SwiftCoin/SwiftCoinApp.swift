18c43ec68de0167ffd4a4387877d2982cd89cd49
asfann <64355174+asfann@users.noreply.github.com>
2022-12-09 14:08:24 +0600

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Asf on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
